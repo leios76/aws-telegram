bc4c5641081500d46f133d75346205891d076c40
Minsoo Kim <leios76@gmail.com>
2019-07-31 17:02:51 +0900

    공통 스크립트 변경

---
+++ init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+NAME=$(basename $(realpath .))
+
+echo "Set version 0.0.1"
+sed -ri "s/\"version\": \"[^\"]+\"/\"version\": \"0.0.1\"/g" nodejs/package.json
+
+echo "Set name ${NAME}"
+sed -ri "s/\"name\": \"[^\"]+\"/\"name\": \"${NAME}\"/g" nodejs/package.json
+
+echo "Remove old git repository"
+rm -Rf .git
+
+echo "Init new git repository"
+git init
+git remote add origin git@github.com:leios76/${NAME}.git
+git flow init -d
