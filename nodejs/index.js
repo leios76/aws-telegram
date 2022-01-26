101be6078775e0735d708ea7543f40791a089000
Minsoo Kim <ms.kim@hdel.co.kr>
2022-01-26 16:04:15 +0900

    소스 구조 변경

--- nodejs/index.js
+++ nodejs/index.js
@@ -2,11 +2,7 @@ const request = require('request');
 const config = require('config');
 const async = require('async');
 const AWS = require('aws-sdk');
-const elec = require('./src/elec.js');
-const giftcard = require('./src/giftcard.js');
-const whooing = require('./src/whooing.js');
-const balance = require('./src/balance.js');
-const test = require('./src/test.js');
+const apiGateway = require('api/gateway.js');
 
 AWS.config.update({
     region: 'ap-northeast-2',
@@ -30,32 +26,6 @@ var req = request.defaults({
     followAllRedirects: true,
     //encoding: null
 });
-
-var sendMessage = function (message, chat_id, callback) {
-    if (message.length > 0 && chat_id > 0) {
-        var telegramConfig = config.get('telegram');
-        var option = {
-            uri: `https://api.telegram.org/${telegramConfig.bot_id}:${telegramConfig.token}/sendMessage`,
-            method: 'POST',
-            json: true,
-            body: {
-                'chat_id': chat_id,
-                'text': message
-            }
-        };
-
-        req(option, function (err, response, body) {
-            if (!err && (body && !body.ok)) {
-                console.log(body);
-                callback("Send Message Fail", { result: "nok" });
-            } else {
-                callback(err, { result: "ok" });
-            }
-        });
-    } else {
-        callback(null, { result: "no message" });
-    }
-};
 
 var setWebhook = function (result, callback) {
     var telegramConfig = config.get('telegram');
@@ -123,111 +93,6 @@ var saveMessage = function (update, response, callback) {
     });
 };
 
-var processMessage = function (update, response, callback) {
-    if (update.message) {
-        console.log(`${update.message.from.last_name} ${update.message.from.first_name}(${update.message.from.username}): ${update.message.text}`);
-        if (!update.message.from.is_bot) {
-            async.eachSeries(update.message.entities, function (entity, callback) {
-                console.log("Process entity:", entity);
-                if (entity.type === "bot_command") {
-                    var args = update.message.text.substring(entity.offset + entity.length).split(' ');
-                    switch (update.message.text.substring(entity.offset, entity.offset + entity.length)) {
-                        case "/giftcard":
-                            giftcard.processCommand(args, function(err, result) {
-                                if (err === null && result !== null) {
-                                    sendMessage(result.message, update.message.chat.id, function (err, result) {
-                                        callback(err);
-                                    });
-                                } else {
-                                    callback(null);
-                                }
-                            });
-                            break;
-                        case "/elec":
-                            elec.processCommand(args, function(err, result) {
-                                if (err === null && result !== null) {
-                                    sendMessage(result.message, update.message.chat.id, function (err, result) {
-                                        callback(err);
-                                    });
-                                } else {
-                                    callback(null);
-                                }
-                            });
-                            break;
-                        case "/whooing":
-                            whooing.processCommand(args, function(err, result) {
-                                if (err === null && result !== null) {
-                                    sendMessage(result.message, update.message.chat.id, function (err, result) {
-                                        callback(err);
-                                    });
-                                } else {
-                                    callback(null);
-                                }
-                            });
-                            break;
-                        case "/balance":
-                            balance.processCommand(args, function(err, result) {
-                                if (err === null && result !== null) {
-                                    sendMessage(result.user.point, update.message.chat.id, function (err, result) {
-                                        callback(err);
-                                    });
-                                } else {
-                                    callback(null);
-                                }
-                            });
-                            break;
-                        case "/test":
-                            test.processCommand(args, function(err, result) {
-                                if (err === null && result !== null) {
-                                    sendMessage(result.message, update.message.chat.id, function (err, result) {
-                                        callback(err);
-                                    });
-                                } else {
-                                    callback(null);
-                                }
-                            });
-                            break;
-                        default:
-                            callback(null);
-                            break;
-                    }
-                } else {
-                    callback(null);
-                }
-            }, function (err) {
-                callback(err, "", 0);
-            });
-
-            return;
-        }
-    }
-
-    callback(null, "", 0);
-};
-
-var telegramCallback = function (event, context, callback) {
-    async.waterfall([
-        function (callback) {
-            callback(null, JSON.parse(event.body), {});
-        },
-        //saveMessage,
-        processMessage,
-        sendMessage,
-    ], function (err, response) {
-        if (err) {
-            console.log(err);
-        }
-
-        callback(err, {
-            "statusCode": 200,
-            "headers": {
-            },
-            "body": JSON.stringify(response),
-            "isBase64Encoded": false
-        });
-    });
-}
-
 exports.webhook = function (event, context, callback) {
     async.waterfall([
         function (callback) {
@@ -253,7 +118,7 @@ exports.handler = function (event, context, callback) {
     if (event.path) {
         // API gateway
         if (event.path === "/telegram/webhook") {
-            telegramCallback(event, context, callback);
+            apiGateway.handler(event, context, callback);
         } else {
             console.log(`Unknown path: ${event.path}`);
         }
