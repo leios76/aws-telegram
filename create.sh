c26e86084566d91d8bdfa0ac5a37348c6c045fea
Minsoo Kim <ms.kim@hdel.co.kr>
2022-03-27 16:29:04 +0900

    aws script 수정

--- create.sh
+++ create.sh
@@ -22,5 +22,5 @@ NAME=$(basename $(realpath .))
 zip -r ${NAME}-modules.zip nodejs/node_modules && \
     LAYER_VERSION=$(aws lambda publish-layer-version --layer-name ${NAME}-modules --zip-file fileb://${NAME}-modules.zip | jq -r .LayerVersionArn) && \
     cd nodejs && zip -r ../${NAME}.zip . -x node_modules\* && cd .. && \
-    aws lambda create-function --function-name ${NAME} --zip-file fileb://${NAME}.zip --handler index.handler --runtime nodejs10.x --role ${ROLE} --timeout 60 --layers ${LAYER_VERSION} --environment Variables="{}" && \
+    aws lambda create-function --function-name ${NAME} --zip-file fileb://${NAME}.zip --handler index.handler --runtime nodejs14.x --role ${ROLE} --timeout 30 --layers ${LAYER_VERSION} --environment Variables="{}" && \
     aws lambda create-alias --function-name ${NAME} --name service --function-version \$LATEST
